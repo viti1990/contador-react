94966342255a56462116f3444baa8c5fe81c78fb
viti1990 <66841282+viti1990@users.noreply.github.com>
2022-01-05 19:39:58 +0100

    Actualizacion estilos css

--- components/Contador.js
+++ components/Contador.js
@@ -3,28 +3,33 @@ const Contador = () => {
     const [contador,setContador] = React.useState(0);
     const [palabra,addpalabra] = React.useState('cuco');
 
-    const prueba = () => {
+    const palabraPrueba = () => {
         console.log('dsa');
         addpalabra('yeso')
     }
+
+
     const aumentar = () =>  {
         setContador(contador + 1);
-   
-}
-   
-const disminuir = () =>  {
-    if(contador>0){
-        setContador(contador - 1);
     }
-}
+   
+    const disminuir = () =>  setContador(contador - 1);
+
 
     return (
         <div>
-            <h1>Contador : {contador}</h1>
-            <h1>Contador : {palabra}</h1>
-            <button onClick={prueba}>Prueba</button>
+            <p class="title">Contador : 
+                <span class={contador < 0 ? "menor" : "mayor"}>
+                    {contador}
+                  </span>
+             </p>
+            <h1>Cambia la palabra : {palabra}</h1>
+
             <button onClick={aumentar}>Aumentar</button>
             <button onClick={disminuir}>Disminuir</button>
+
+            <button onClick={palabraPrueba}>Cambia la palabra</button>
+
 
         </div>
     )
